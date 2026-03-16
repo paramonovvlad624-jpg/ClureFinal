b2c4bf978e8cf377719c0ee8c5c7b484558d60ca
Clure <cluremag@yandex.ru>
2026-03-16 20:02:41 +0300

    16

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,67 @@ import '@fontsource/geist-sans/500.css'
 import '@fontsource/geist-sans/600.css'
 import '@fontsource/geist-sans/900.css'
 import { Analytics } from '@vercel/analytics/next'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/favicon-16/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon-16/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon-16/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon-16/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Clure" />
+        <link rel="manifest" href="/images/favicon-16/site.webmanifest" />
+        {/* Structured data to help search engines show site sections (replace example.com with your site URL) */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Clure",
+              "url": "https://example.com",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://example.com/search?q={search_term_string}",
+                "query-input": "required name=search_term_string"
+              },
+              "publisher": {
+                "@type": "Organization",
+                "name": "Clure"
+              },
+              "mainEntity": [
+                {
+                  "@type": "SiteNavigationElement",
+                  "name": "Home",
+                  "url": "https://example.com/"
+                },
+                {
+                  "@type": "SiteNavigationElement",
+                  "name": "About",
+                  "url": "https://example.com/about"
+                },
+                {
+                  "@type": "SiteNavigationElement",
+                  "name": "Articles",
+                  "url": "https://example.com/articles"
+                },
+                {
+                  "@type": "SiteNavigationElement",
+                  "name": "Playlists",
+                  "url": "https://example.com/playlists"
+                },
+                {
+                  "@type": "SiteNavigationElement",
+                  "name": "Theory Fest",
+                  "url": "https://example.com/theory-fest"
+                }
+              ]
+            })
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
